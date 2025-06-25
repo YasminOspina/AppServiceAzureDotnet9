76f9ea35abaff8befb3fc6f53dda3829f0b070f7
YasminOspina <sirleyob244@gmail.com>
2025-06-25 18:23:47 -0500

    Deploy appservice github actions

--- obj/Debug/net9.0/AppServiceAzureDotnet9.AssemblyInfo.cs
+++ obj/Debug/net9.0/AppServiceAzureDotnet9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppServiceAzureDotnet9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaa98ac5cd176bcbf537cfb37132fc9040d67e40")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppServiceAzureDotnet9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppServiceAzureDotnet9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
